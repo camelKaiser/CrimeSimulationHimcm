60889e4fca337a4483a92c90dbd4bc0e0ac5198c
lemonFlute <wzli98@gmail.com>
2015-12-28 21:00:46 -0500

    family class

---
+++ Family.java
@@ -0,0 +1,40 @@
+
+public class Family 
+{
+	private int a; //IDs of the 4 nodes 
+	private int b;
+	private int c;
+	private int d;
+	
+	public Family() //constructors
+	{
+		
+	}
+	public Family(int a, int b, int c,int d)
+	{
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		this.d=d;
+		
+		
+		
+	}
+	//accessors
+	public int a()
+	{
+		return a;
+	}
+	public int b()
+	{
+		return b;
+	}
+	public int c()
+	{
+		return c;
+	}
+	public int d()
+	{
+		return d;
+	}
+}
